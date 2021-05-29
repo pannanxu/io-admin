cbe6726172b5af78212fb463b3d5eb216051fbfa
Administrator <admin@example.com>
2021-05-29 11:53:37 +0800

    add global cors

---
+++ io-common/io-common-core/src/main/java/io/mvvm/config/GlobalCorsConfig.java
@@ -0,0 +1,32 @@
+package io.mvvm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @program: io-admin
+ * @description: 跨域配置
+ * @author: Mr. Pan
+ * @create: 2021-05-29 11:21
+ **/
+public class GlobalCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        //允许所有域名进行跨域调用
+        config.addAllowedOrigin("*");
+        //允许跨越发送cookie
+        config.setAllowCredentials(true);
+        //放行全部原始头信息
+        config.addAllowedHeader("*");
+        //允许所有请求方法跨域调用
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
